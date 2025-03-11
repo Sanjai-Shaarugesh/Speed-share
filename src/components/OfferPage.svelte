31588e1b582d8d78fcbab75502229245fd3b875e
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-03-11 16:49:55 +0530

    few  changes ui 🍦

--- src/components/OfferPage.svelte
+++ src/components/OfferPage.svelte
@@ -183,33 +183,25 @@
         {#if showOfferOptions}
           <OfferOptions onUpdate={onOptionsUpdate} />
         {/if}
-        <div class="mt-4 flex flex-row gap-2">
+        <div class="">
           
           
-          <a href="#_"  onclick={generateOfferCode} class="relative  items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
-              <span  class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
-              <span  class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
-                  <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
-                  <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
-              </span>
-              <span class="relative text-white">Generate Offer Code</span>
-          </a>
+            
+            
+            <button class="btn btn-soft btn-secondary"  onclick={generateOfferCode}>Generate Offer Code</button>
           
           {#if !showOfferOptions}
             
             
-            <a href="#_"  onclick={() => {
-              showOfferOptions = true;
-            }} class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-red-500 rounded-xl group">
-                <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4">
-                    <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
-                </span>
-                <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
-                <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">Settings</span>
-            </a>
+             
+              
+              <button class="btn btn-secondary" onclick={() => {
+   showOfferOptions = true;
+ }}>Settings</button>
             
           {/if}
-          <button class="btn btn-outline" onclick={navigateToAnswerPage}>Go to Answer Page</button>
+          
+          <button class="btn btn-dash btn-warning" onclick={navigateToAnswerPage}>Go to Answer Page</button>
         </div>
       </div>
     {/if}
