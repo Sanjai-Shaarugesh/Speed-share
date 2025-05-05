f89ca393cbd520772a03f5e90bc537b97436752a
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-05-05 21:12:32 +0530

    bugs fixed in rendering page

--- src/components/ContainerTextFlipDemo.jsx
+++ src/components/ContainerTextFlipDemo.jsx
@@ -1,25 +1,27 @@
 import { ContainerTextFlip } from "@/components/ui/container-text-flip";
 import { useState, useEffect } from 'react';
- import { Capacitor } from '@capacitor/core';
+import { Capacitor } from '@capacitor/core';
 
 export function ContainerTextFlipDemo() {
   
-  let [isNative , setIsNative] = useState(false);
+  const [platformChecked, setPlatformChecked] = useState(false);
+  const [isNative, setIsNative] = useState(true); 
   
-  useEffect(()=>{
+  useEffect(() => {
+    
     setIsNative(Capacitor.isNativePlatform());
-  })
+    
+    setPlatformChecked(true);
+  }, []); 
   
   return (
-    <div className="flex items-center justify-center ">
-     {!isNative ? (
-       <ContainerTextFlip 
-         words={["better", "modern", "faster", "smarter" ,"secure", "reliable"]} 
-         className="w-48 h-16 my-8"  
-       />
-     ) : (
-     null
-     )}
+    <div className="flex items-center justify-center">
+      {platformChecked && !isNative && (
+        <ContainerTextFlip 
+          words={["better", "modern", "faster", "smarter", "secure", "reliable"]} 
+          className="w-48 h-16 my-8"  
+        />
+      )}
     </div>
   );
 }
