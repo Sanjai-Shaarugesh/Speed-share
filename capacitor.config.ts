4569c3834671637d66fb39b55eab5a7c45c7d8aa
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-05-06 22:39:59 +0530

    bugs fixes for android apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,34 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.Speed-share.app',
+  appId: 'com.speed_share.app',
   appName: 'Speed-share',
   webDir: './build',
   server: {
     androidScheme: 'https',
     allowNavigation: ['*'] // allow all internal routes like /offer, /answer
-  }
+  },
+  plugins: {
+      SplashScreen: {
+        launchShowDuration: 1000,
+        launchAutoHide: true,
+        launchFadeOutDuration: 3000,
+        backgroundColor: "#00000000",
+        androidSplashResourceName: "splash",
+        androidScaleType: "CENTER_CROP",
+        showSpinner: true,
+        androidSpinnerStyle: "large",
+        iosSpinnerStyle: "small",
+        spinnerColor: "#999999",
+        splashFullScreen: true,
+        splashImmersive: true,
+        layoutName: "launch_screen",
+        useDialog: true,
+      },
+    },
+    android: {
+      allowMixedContent: true
+    }
 };
 
 export default config;
