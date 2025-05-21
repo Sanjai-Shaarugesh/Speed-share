0e3ac05885422b60a06eabb9b0a6b2f7f00fda63
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-05-21 12:13:24 +0530

    QR code bug 🪲 fixed

--- src/components/qr/QrModal.svelte
+++ src/components/qr/QrModal.svelte
@@ -51,11 +51,19 @@
   <QrCode /> QR Code
 </label>
 
+
 <input type="checkbox" id="qr-modal" class="modal-toggle" bind:checked={isModalOpen} />
 
-<label for="qr-modal" class="modal cursor-pointer">
-  <label class="modal-box relative flex flex-col justify-center items-center w-auto" for="">
+<label for="qr-modal" class="modal cursor-pointer  bg-opacity-40 backdrop-blur-md">
+  <label class="modal-box relative flex flex-col justify-center items-center w-auto " for="">
+    <button
+    class="btn btn-sm btn-circle absolute right-2 top-2 bg-pink-500 text-black "
+    onclick={() => (isModalOpen = false)}
+    aria-label="Close"
+  >
+    ✕
+  </button>
     <h3 class="text-lg font-bold">{title}</h3>
-    <img class="w-screen" src={qrcode.toDataURL()} alt="qr" />
+    <img class="w-full max-w-96 rounded-xl border-4 border-dashed bg-origin-content p-1 " src={qrcode.toDataURL()} alt="qr" />
   </label>
 </label>
